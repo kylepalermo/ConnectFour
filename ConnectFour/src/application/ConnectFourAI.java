eefb4ba5bd379b98a2e7ade355cb9c744d2b852c
Kyle Palermo <kylepalermo@tamu.edu>
2023-10-31 23:37:47 -0500

    Renamed ConnectFourAI class definition

--- ConnectFour/src/application/ConnectFourAI.java
+++ ConnectFour/src/application/ConnectFourAI.java
@@ -2,7 +2,7 @@ package application;
 
 // stores functions that return easy, hard, and master level AI moves
 
-public class ConnectFourAi {
+public class ConnectFourAI {
 	// returns destination cell of easy AI's move, or 0 if there are no moves available
 	// easy AI makes and defends immediate wins, but is clueless otherwise
 	// TODO: not yet implemented
