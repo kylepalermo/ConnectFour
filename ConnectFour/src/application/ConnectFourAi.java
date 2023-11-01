95b0e94a2ab5cbb760a5fe9fca4a89dfa8639e8b
Kyle Palermo <kylepalermo@tamu.edu>
2023-10-31 23:32:51 -0500

    Added ConnectFourAI class

---
+++ ConnectFour/src/application/ConnectFourAi.java
@@ -0,0 +1,24 @@
+package application;
+
+// stores functions that return easy, hard, and master level AI moves
+
+public class ConnectFourAi {
+	// returns destination cell of easy AI's move, or 0 if there are no moves available
+	// easy AI makes and defends immediate wins, but is clueless otherwise
+	// TODO: not yet implemented
+	public long easyMove(long player1Data, long player2Data) {
+		return 0;
+	}
+	// returns destination cell of hard AI's move, or 0 if there are no moves available
+	// hard AI makes and defends immediate wins and aggressively sets up wins
+	// TODO: not yet implemented
+	public long hardMove(long player1Data, long player2Data) {
+		return 0;
+	}
+	// returns destination cell of hard AI's move, or 0 if there are no moves available
+	// master AI makes optimal moves according to the solved game strategy
+	// TODO: not yet implemented
+	public long masterMove(long player1Data, long player2Data) {
+		return 0;
+	}
+}
